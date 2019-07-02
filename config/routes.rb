d1513c9a96b81a18c93ede2c23b99504f01acb80
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-02 05:21:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :artists, only: [:index, :show, :create, :update, :new, :edit, :destroy]
     resources :songs, only: [:index, :show, :create, :update, :new, :edit, :destroy]
+    resources :genres, only: [:index, :show, :create, :update, :new, :edit, :destroy]
 
   # Example resource route with options:
   #   resources :products do
